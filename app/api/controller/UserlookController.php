0a4d1a4ba676c0e0d9b854926d37d0ab3585657f
Hao <913812595@qq.com>
2020-06-01 14:11:38 +0800

    谁看过我接口

---
+++ app/api/controller/UserlookController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\api\controller;
+
+use app\admin\model\UserModel;
+use think\Db;
+
+class UserlookController
+{
+    /**
+     * 我的模块添加谁看过我接口
+     * user_id 用户id
+     * user_id_look 谁看过我用户id
+     */
+    public function add()
+    {
+        $user_id = request()->post('user_id');
+        $user_id_look = request()->post('user_id_look');
+        $user_id = isset($user_id) ? $user_id : '';
+        $user_id_look = isset($user_id_look) ? $user_id_look : '';
+        if (empty($user_id || $user_id_look)) {
+            return json(array('error' => 1, 'errorMsg' => '请求失败'));
+        }
+        $data['user_id'] = $user_id;
+        $data['user_id_look'] = $user_id_look;
+        $data['look_time'] = time();
+        $user = Db::name('user_look')->insert($data);
+        if ($user) {
+            return json(array('error' => 0, 'errorMsg' => '请求成功', 'data' => $data));
+        } else {
+            return json(array('error' => 1, 'errorMsg' => '请求失败'));
+        }
+    }
+
+    /**
+     * 我的模块谁看过我列表接口
+     * user_id 用户自增id
+     */
+    public function list()
+    {
+        $request = request();
+        $page = $request->post('page');
+        $page_num = 10;
+        $user_id = $request->post('user_id');
+        $user_id = isset($user_id) ? $user_id : '';
+        if (empty($user_id)) {
+            return json(array('error' => 1, 'errorMsg' => '请求失败'));
+        }
+        $time = strtotime("-30 day");
+        $look_users = Db::name('user_look')
+            ->alias('a')
+            ->join('user b','a.user_id_look=b.id','LEFT')
+            ->where('a.user_id', $user_id)
+            ->where('a.look_time','>',$time)
+            ->field('a.look_time,b.id,b.nickname,b.avatar,b.sex')
+            ->page($page,$page_num)
+            ->select();
+        $data = [];
+        foreach($look_users as $k=>$v){
+            $data[$k]['avatar'] = cmf_get_image_preview_url($v['avatar']);
+            $data[$k]['user_id'] = $v['id'];
+            $data[$k]['nickname'] = $v['nickname'];
+            $data[$k]['sex'] = $v['sex'];
+            $data[$k]['look_time'] = get_time($v['look_time']);
+        }
+        if ($data) {
+            return json(array('error' => 0, 'errorMsg' => '请求成功', 'data' => $look_users));
+        } else {
+            return json(array('error' => 1, 'errorMsg' => '请求失败'));
+        }
+    }
+}
--- simplewind/cmf/common.php
+++ simplewind/cmf/common.php
@@ -1948,3 +1948,37 @@ function isMobile($value)
         return false;
     }
 }
+/**
+ * 判断今天昨天前天
+ *
+ * @param [type] $targetTime
+ * @return void
+ */
+function get_time($targetTime)
+{
+    // 今天最大时间
+    $todayLast=strtotime(date('Y-m-d 23:59:59'));
+    $agoTimeTrue = time() - $targetTime;
+    $agoTime = $todayLast - $targetTime;
+    $agoDay = floor($agoTime / 86400);
+
+    if ($agoTimeTrue < 60) {
+        $result = '刚刚';
+    } elseif ($agoTimeTrue < 3600) {
+        $result = (ceil($agoTimeTrue / 60)) . '分钟前';
+    } elseif ($agoTimeTrue < 3600 * 12) {
+        $result = (ceil($agoTimeTrue / 3600)) . '小时前';
+    } elseif ($agoDay == 0) {
+        $result = '今天 ' . date('H:i', $targetTime);
+    } elseif ($agoDay == 1) {
+        $result = '昨天 ' . date('H:i', $targetTime);
+    } elseif ($agoDay == 2) {
+        $result = '前天 ' . date('H:i', $targetTime);
+    } elseif ($agoDay > 2 && $agoDay < 16) {
+        $result = $agoDay . '天前 ' . date('H:i', $targetTime);
+    } else {
+        $format = date('Y') != date('Y', $targetTime) ? "Y-m-d H:i" : "m-d H:i";
+        $result = date($format, $targetTime);
+    }
+    return $result;
+}
